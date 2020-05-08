0f42730fe4a80c4956687c8bd15ad9c6a6191c99
Daniil Solyanoy <39529339+SP1K33@users.noreply.github.com>
2020-05-08 16:49:35 +0300

    Removed extra

--- NetworkJitter/NetworkJitter/MainForm.Designer.cs
+++ NetworkJitter/NetworkJitter/MainForm.Designer.cs
@@ -1,16 +1,11 @@
-﻿namespace NetworkJitter
+﻿using System;
+
+namespace NetworkJitter
 {
-	partial class Form1
+	partial class MainForm
 	{
-		/// <summary>
-		/// Обязательная переменная конструктора.
-		/// </summary>
 		private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		/// Освободить все используемые ресурсы.
-		/// </summary>
-		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
 		protected override void Dispose(bool disposing)
 		{
 			if (disposing && (components != null))
@@ -20,21 +15,24 @@
 			base.Dispose(disposing);
 		}
 
-		#region Код, автоматически созданный конструктором форм Windows
+		private void MainForm_Load(object sender, EventArgs e) {}
 
-		/// <summary>
-		/// Требуемый метод для поддержки конструктора — не изменяйте 
-		/// содержимое этого метода с помощью редактора кода.
-		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.SuspendLayout();
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(457, 165);
+			this.Name = "MainForm";
 			this.Text = "Form1";
-		}
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.ResumeLayout(false);
 
-		#endregion
+
+		}
 	}
 }
 
