17d7d1bdd1e042101f8e50154f93462f5f9bb2c4
Daniil Solyanoy <39529339+SP1K33@users.noreply.github.com>
2020-05-08 16:54:16 +0300

    AppController

---
+++ NetworkJitter/NetworkJitter/AppController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace NetworkJitter
+{
+    public class AppController
+    {
+        #region Singleton
+        private static AppController _instance;
+
+        public static AppController Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new AppController();
+                }
+                return _instance;
+            }
+        }
+        #endregion
+
+        private string GetAdapterName()
+        {
+            Console.Write("Enter adapter name: ");
+            string userInput = Console.ReadLine();
+            string result = string.IsNullOrEmpty(userInput) ? "Ethernet" : userInput;
+            Console.WriteLine($"Adapter name: {result}");
+            return result;
+        }
+
+        private int GetPauseTime()
+        {
+            Console.Write("Enter pause (ms): ");
+            string userInput = Console.ReadLine();
+            int result = (string.IsNullOrEmpty(userInput)) ? 3000 : int.Parse(userInput);
+            Console.WriteLine($"Pause time: {result}");
+            return result;
+        }
+
+        public void RunStartup()
+        {
+            string adapterName = GetAdapterName();
+            int pauseTime = GetPauseTime();
+        }
+
+    }
+}
